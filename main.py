db0ef3f043714125ac2ce821f7abfb8111eae5f4
kingsleylow0327 <kingsleylow94@gmail.com>
2023-02-10 17:50:47 +0800

    map walking algo

--- main.py
+++ main.py
@@ -54,4 +54,30 @@ for item in raw_array:
 
     filtered_lines.append([c for c in raw_line])
 
+# Main Counting Logic
+def island_count(map) -> int:
+    if len(map) < 1:
+        return "This is an empty map"
 
+    col = len(map)
+    row = len(map[0])
+    island_num = 0
+
+    for y in range(col):
+        for x in range(row):
+            if map[y][x] == '1':
+                walk(y, x, map)
+                island_num += 1
+    return island_num
+
+# Map Walking logic
+def walk(y, x, map):
+    # Prevent walk out the map
+    if y < 0 or y >= len(map) or x < 0 or x >= len(map[y]):
+        return
+    if map[y][x] == '1':
+        map[y][x] = '0' # Set 1 to 0 to prevent come back again
+        walk(y-1, x, map) # up
+        walk(y+1, x, map) # down
+        walk(y, x-1, map) # left
+        walk(y, x+1, map) # right
