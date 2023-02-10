09c9a35324b197e44606aa2c64f52a99646eeeea
kingsleylow0327 <kingsleylow94@gmail.com>
2023-02-10 21:28:33 +0800

    flake8 adaptation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import re
 import sys
 from pathlib import Path
 
-def map_validator (raw_array, input_path):
+
+def map_validator(raw_array, input_path):
     filtered_lines = []
     line_len = 0
 
@@ -16,19 +16,20 @@ def map_validator (raw_array, input_path):
             print(f"Shape of '{input_path}' is not x*y")
             filtered_lines = []
             break
-        
+
         # Check for empty lines
         if raw_line == "":
             print(f"'{input_path}' contained exessive empty lines")
             break
-        
+
         # Check for characters
         if not all(ch in "01" for ch in raw_line):
-            print(f"'{input_path}' contained characters other than '0' and '1'")
+            print(f"'{input_path}' contained characters other than '0' & '1'")
             break
 
         filtered_lines.append([c for c in raw_line])
     return filtered_lines
+
 
 # Main Counting Logic
 def island_count(map) -> int:
@@ -46,24 +47,26 @@ def island_count(map) -> int:
                 island_num += 1
     return island_num
 
+
 # Map Walking logic
 def walk(y, x, map):
     # Prevent walk out the map
     if y < 0 or y >= len(map) or x < 0 or x >= len(map[y]):
         return -1
     if map[y][x] == '1':
-        map[y][x] = '0' # Set 1 to 0 to prevent come back again
-        walk(y-1, x, map) # up
-        walk(y+1, x, map) # down
-        walk(y, x-1, map) # left
-        walk(y, x+1, map) # right
+        map[y][x] = '0'  # Set 1 to 0 to prevent come back again
+        walk(y-1, x, map)  # up
+        walk(y+1, x, map)  # down
+        walk(y, x-1, map)  # left
+        walk(y, x+1, map)  # right
+
 
 def main():
     # Initialize Variable
     raw_array = []
 
     # Python script argument
-    if len(sys.argv) <=1 :
+    if len(sys.argv) <= 1:
         print("Missing Text Path. Try: main.py <input>.txt")
         return "Error"
 
@@ -78,7 +81,7 @@ def main():
         return "Error"
 
     # Read from txt file
-    with open (input_path) as f:
+    with open(input_path) as f:
         raw_array = f.readlines()
 
     # Check raw_array is not empty:
@@ -88,9 +91,10 @@ def main():
 
     # Checking 2D array
     map = map_validator(raw_array, input_path)
-    
+
     island_number = island_count(map)
     return island_number
+
 
 if "__main__" == __name__:
     print(main())
